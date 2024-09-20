058366456e9c8d680f051ff9ac7785a5a92c490e
Fernando Barrionuevo <fernandoenzotb@gmail.com>
2024-09-19 21:46:25 -0300

    preentrega 2+barrionuevo

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,34 @@
-const destino = prompt("Ingrese su destino de vuelo");
-let precio ;
+let destino = prompt("Ingrese su destino de vuelo o ingrese 'salir' en caso de que las promociones vigentes no sean de su interés");
+let precio;
 
+// Array de destinos, cada destino es un objeto con su nombre y precio
+const destinos = [
+    { nombre: "españa", precio: 600 },
+    { nombre: "rusia", precio: 400 },
+    { nombre: "inglaterra", precio: 800 },
+    { nombre: "portugal", precio: 750 }
+];
 
-const españa = 600;
-const rusia = 400;
-const inglaterra = 800;
-const portugal = 750;
+while (destino !== "salir") {
+    // Buscamos el destino en el array
+    const destinoSeleccionado = destinos.find(d => d.nombre === destino.toLowerCase());
 
-if (destino === "españa"){
-    precio = españa
+    if (destinoSeleccionado) {
+        precio = destinoSeleccionado.precio;
+        console.log("Su destino es: " + destino);
+        console.log("El precio del vuelo con descuento es: $" + valorFinal(precio));
+
+    } else {
+        alert("El país ingresado no es válido");
+    }
+
+    // Pedimos el destino de nuevo
+    destino = prompt("Seleccione otro destino de vuelo o 'salir' para terminar");
 }
-else if (destino === "rusia"){
-    precio = rusia
-}
-else if (destino === "inglaterra"){
-    precio = inglaterra
-}
-else if (destino === "portugal"){
-    precio = portugal
-}
-else {
-    alert("El pais ingresado no es valido")
-}
 
-function valorFinal (){
-    const valorDescuento = precio * 30 / 100
-
-    const valorTotal = precio - valorDescuento
-
+// Función que calcula el valor final con descuento
+function valorFinal(precio) {
+    const valorDescuento = precio * 30 / 100;
+    const valorTotal = precio - valorDescuento;
     return valorTotal;
 }
-
-valorFinal()
-
-console.log(valorTotal);
-
-
